966368cf65ab4cf89fc3ff6541211de434fe43f5
Emre Oztunc <emre.oztunc@gmail.com>
2011-07-25 22:06:57 +0300

    These changes are not meaningful. Submodule change is arbitrary.

---
+++ iett/js/models/EventManager.js
@@ -0,0 +1,23 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: emre
+ * Date: 7/25/11
+ * Time: 9:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+iett.EventManager = function() {
+    subscribers = [{}];
+};
+
+
+iett.EventManager.prototype.publish = function(event){
+
+};
+
+
+iett.EventManager.prototype.subscribe = function(eventkey, event, callback){
+
+};
+
